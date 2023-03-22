b1dd5eac44ca4cdf174560579555a7851d560f0d
Michael Shan <1016658371@qq.com>
2023-03-22 17:05:47 +0800

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Grid, Show, GridItem } from '@chakra-ui/react';
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //devices wider than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="dodgerblue">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="green">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
