b123c9ad63540a6c4fd7fbdf2bd969d5699d7b79
Michael Shan <1016658371@qq.com>
2023-03-23 16:08:03 +0800

    build game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,36 +1,21 @@
-import { Text } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGame {
-  count: number;
-  results: Game[];
-}
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import useGames from '../hooks/useGames';
+import GameCard from './GameCard';
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGame>('/games')
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding={10}
+        spacing={10}
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game}></GameCard>
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,36 @@
+import { CanceledError } from 'axios';
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+
+interface FetchGame {
+  count: number;
+  results: Game[];
+}
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    // set cancel HTTP request
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGame>('/games', { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
